05d3d3b4d183d575266bc07f610d31f18feb5a10
Deivid Santos <deivid@deividroger.net>
2023-09-07 01:06:58 -0300

    finalizado módulo de sandbox

--- src/FC.CodeFlix.Catalog.Infra.Data.EF/Migrations/20230907035649_data-structure.Designer.cs
+++ src/FC.CodeFlix.Catalog.Infra.Data.EF/Migrations/20230907035649_data-structure.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FC.CodeFlix.Catalog.Infra.Data.EF;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FC.CodeFlix.Catalog.Infra.Data.EF.Migrations
 {
     [DbContext(typeof(CodeFlixCatalogDbContext))]
-    partial class CodeFlixCatalogDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230907035649_data-structure")]
+    partial class datastructure
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
